96d8dde8fb3f41f7511f3dc1ef0f1dc1b6367d3f
Shezan Abdul Mujeer Chisty <sa24bd@fsu.edu>
2026-05-20 16:56:29 -0400

    Added function to analyze website

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv #Reads necessary values from .env (in this case API Key)
+from tools import web_catcher
 
 while True:
 
@@ -11,11 +12,15 @@ while True:
 
     model = genai.GenerativeModel("gemini-2.5-flash") #Specifies which model of gemini api to use
 
-    user = input("ME: ") #Takes input from user
+    user = input("Enter website to analyze: ") #Takes input from user
 
-    response = model.generate_content(user) #Generates a response based on user input
+    website = web_catcher(user) #Calls the function and provides the URL value
+
+    response = model.generate_content(website) #Generates a response based on user input
+
 
     print ("AI:", response.text) #prints response 
+
 
     if user == "quit": #If user quits this breaks the loop
         break
--- tools.py
+++ tools.py
@@ -1,1 +1,14 @@
 #Fucntions for agent to use
+import requests 
+from bs4 import BeautifulSoup
+
+
+def web_catcher (url): #Function to analyze a given website
+
+    response = requests.get(url) #goes to the webpage and downloads it 
+
+    html = response.text #HTML code from downloaded page
+
+    soup = BeautifulSoup(html, "html.parser") #understands HTML like a webpage structure
+
+    return soup.text #returns value of soup
