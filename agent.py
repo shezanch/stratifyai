6b8b486b757268f3f8a9b1a78e22f105fc5ce32b
Shezan Abdul Mujeer Chisty <sa24bd@fsu.edu>
2026-05-21 16:51:03 -0400

    Added input validation and improved API error handling

--- agent.py
+++ agent.py
@@ -3,24 +3,48 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv #Reads necessary values from .env (in this case API Key)
 from tools import web_catcher
+from google.api_core.exceptions import ResourceExhausted
+
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY")) #API Key configured once. Uses this api key from .env whenever gemini is called
+
+model = genai.GenerativeModel("gemini-2.5-flash") #Specifies which model of gemini api to use
 
 while True:
 
-    load_dotenv()
-
-    genai.configure(api_key=os.getenv("GEMINI_API_KEY")) #API Key configured once. Uses this api key from .env whenever gemini is called
-
-    model = genai.GenerativeModel("gemini-2.5-flash") #Specifies which model of gemini api to use
-
     user = input("Enter website to analyze: ") #Takes input from user
+    user = user.strip()
+    
+#Input Validations
+    if user == "": #Did the user type anything at all
+        print ("You must enter a website")
+        continue
 
+    elif user == "quit": #If user quits this breaks the loop
+        break
+    
+    elif not user.startswith("http://") and not user.startswith("https://"): #Does it look like a URL?
+        print ("You must enter a valid website starting with http:// or https://")
+        continue
+
+    
     website = web_catcher(user) #Calls the function and provides the URL value
 
-    response = model.generate_content(website) #Generates a response based on user input
+    if website == "Enter a valid website": #Checks validation from tools if website is valid
+        print (website)
+        continue
+    elif website == "Website not found":
+        print (website)
+        continue
 
-
-    print ("AI:", response.text) #prints response 
-
-
-    if user == "quit": #If user quits this breaks the loop
-        break
+    try:
+        response = model.generate_content(website) #Generates a response based on user input
+    except ResourceExhausted:
+        print ("Api rate limit reached. Try again later")
+    except:
+        print ("ApI failed. Try agina later")
+    else:
+        print ("AI:", response.text)
+       
--- tools.py
+++ tools.py
@@ -5,10 +5,17 @@ from bs4 import BeautifulSoup
 
 def web_catcher (url): #Function to analyze a given website
 
-    response = requests.get(url) #goes to the webpage and downloads it 
+    try:
 
-    html = response.text #HTML code from downloaded page
-
-    soup = BeautifulSoup(html, "html.parser") #understands HTML like a webpage structure
-
-    return soup.text #returns value of soup
+        response = requests.get(url) #goes to the webpage and downloads it 
+    
+    except:
+        return "Enter a valid website"
+    
+    else: 
+        if response.status_code != 200:
+            return "Website not found"
+        
+        html = response.text #HTML code from downloaded page
+        soup = BeautifulSoup(html, "html.parser") #understands HTML like a webpage structure
+        return soup.text #returns value of soup
